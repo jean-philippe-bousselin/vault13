aa381f2d8780e63c4e5ffca3c48e5b8d3dfcc534
jpbousselin <jean-philippe.bousselin@ueberbit.de>
2015-02-26 09:22:13 +0100

    layout tests

---
+++ src/client/views/feed/feed.js
@@ -0,0 +1,11 @@
+Template.feed.helpers({
+});
+
+Template.feed.events({
+});
+
+Template.feed.rendered = function() {
+    $('.special.cards .image').dimmer({
+        on: 'hover'
+    });
+};
--- src/server/config/security.js
+++ src/server/config/security.js
@@ -2,5 +2,6 @@ BrowserPolicy.content.allowOriginForAll("*.googleapis.com");
 BrowserPolicy.content.allowOriginForAll("*.gstatic.com");
 BrowserPolicy.content.allowOriginForAll("*.bootstrapcdn.com");
 BrowserPolicy.content.allowOriginForAll("*.youtube.com");
+BrowserPolicy.content.allowOriginForAll("*.findicons.com");
 
 BrowserPolicy.content.allowFontDataUrl();

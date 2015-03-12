970d2363d54d6c4eeb2556412316f136b88e2c87
jpbousselin <jean-philippe.bousselin@ueberbit.de>
2015-03-12 13:34:27 +0100

    login enhanced

--- src/client/views/login/login.js
+++ src/client/views/login/login.js
@@ -1,14 +1,27 @@
+Template.login.helpers({
+
+    'accessDenied': function() {
+        return typeof Session.get('accessDenied') != 'undefined';
+    }
+
+});
+
 Template.login.events({
     "submit #login-form": function(event, template) {
         event.preventDefault();
-        Meteor.loginWithPassword(
-            template.find("#login-username").value,
-            template.find("#login-password").value,
-            function(error) {
-                if (error) {
-                    // Display the login error to the user however you want
+        var username = template.find("#login-username").value;
+        var password = template.find("#login-password").value;
+
+        if(username.length > 0 && password.length > 0) {
+            Meteor.loginWithPassword(
+                username,
+                password,
+                function(error) {
+                    if (error) {
+                        Session.set('accessDenied', true);
+                    }
                 }
-            }
-        );
+            );
+        }
     }
 });

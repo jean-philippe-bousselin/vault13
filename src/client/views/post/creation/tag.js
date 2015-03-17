1b233a34ea2d1d1a312aa075f9d41f4a81bd0e02
blackcat <jp.bousselin@gmail.com>
2015-03-18 00:12:21 +0100

    added missing files

---
+++ src/client/views/post/creation/tag.js
@@ -0,0 +1,18 @@
+var selectedTag, newPost;
+Template.tag.events({
+
+    'click .artist-tag': function() {
+        // delete the tag on click over it
+        newPost = Session.get('newPost');
+        selectedTag = this;
+        $.each(newPost.resource.tags, function(index, tag){
+            if(tag.name == selectedTag.name) {
+                delete newPost.resource.tags[index];
+                // reset keys
+                newPost.resource.tags = newPost.resource.tags.filter(function(){return true;});
+                Session.set('newPost', newPost);
+            }
+        });
+
+    }
+});

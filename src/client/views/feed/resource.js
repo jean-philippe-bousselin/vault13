33b8230fb7aaff24628fd70dbe1fda970bec1383
blackcat <jp.bousselin@gmail.com>
2015-03-17 22:39:22 +0100

    add unicity on the playlist

--- src/client/views/feed/resource.js
+++ src/client/views/feed/resource.js
@@ -2,8 +2,17 @@ var playlistArray;
 
 Template.resource.events({
     'click .add-to-playlist': function(event, element) {
+        var selectedResource = this.resource;
         playlistArray = Session.get('playlist');
+        // remove duplicates from the list if any (dupes are based on the url)
+        $.each(playlistArray, function(key, resource){
+            if(resource.originalUrl == selectedResource.originalUrl) {
+                delete playlistArray[key];
+            }
+        });
         playlistArray.push(this.resource);
+        // reset keys
+        playlistArray = playlistArray.filter(function(){return true;});
         Session.set('playlist', playlistArray);
         // play resource if play button
         if(event.currentTarget.classList.contains('trigger-play')) {

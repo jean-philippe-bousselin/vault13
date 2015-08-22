07b72a28122fca4c33eeeaca81984fe004e97775
jean-philippe-bousselin <jp.bousselin@gmail.com>
2015-08-22 18:08:52 +0200

    * added notifications on new posts * changed scroll buttons style

--- src/client/views/components/scrollButtons/scrollButtons.js
+++ src/client/views/components/scrollButtons/scrollButtons.js
@@ -11,6 +11,19 @@ Template.scrollButtons.events({
     }
 });
 
+Template.scrollButtons.helpers({
+
+    'newPostsNotifications': function() {
+        return Session.get('postCreatedNotifications');
+    },
+    'newPosts': function() {
+        return Session.get('postCreatedNotifications') > 0;
+    },
+    'text': function() {
+        return Session.get('postCreatedNotifications') > 1? 'New posts' : 'New post';
+    }
+});
+
 Template.scrollButtons.rendered = function() {
     var scrolltop;
     $('.dynamic-content').scroll(function(e){
@@ -18,7 +31,9 @@ Template.scrollButtons.rendered = function() {
         if(scrolltop > 50) {
             $('.omnibox-control-buttons').fadeIn('fast');
         } else {
-            $('.omnibox-control-buttons').fadeOut('fast');
+            $('.omnibox-control-buttons').fadeOut('fast', function() {
+                Session.set('postCreatedNotifications', 0);
+            });
         }
         $('.omnibox-control-buttons').css('top', scrolltop);
     });
--- src/client/views/post/posts.js
+++ src/client/views/post/posts.js
@@ -1,3 +1,19 @@
 Template.posts.rendered = function() {
     Session.setDefault('postsAutorunRunning', false);
-}
+};
+
+Template.posts.rendered = function () {
+
+    Session.setDefault('postCreatedNotifications', 0);
+
+    var initialized = false;
+    this.data.posts.observeChanges({
+        added: function (id, fields) {
+            if(initialized) {
+                Session.set('postCreatedNotifications', Session.get('postCreatedNotifications') + 1);
+            }
+        }
+    });
+    initialized = true;
+
+};
